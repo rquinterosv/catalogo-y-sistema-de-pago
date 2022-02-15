e800f44fb2f85ce7c8944c75d82959173e191a5d
rquinterosv <rafael.quinterosv@gmail.com>
2022-02-15 20:18:45 -0300

    se cambio home por shops y se modifico el index y show de shops

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :shops, only:[:index, :show]
   resources :products
-  root 'home#index'
+  root 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20220210195700_create_homes.rb
+++
@@ -1,9 +0,0 @@
-class CreateHomes < ActiveRecord::Migration[6.1]
-  def change
-    create_table :homes do |t|
-      t.string :index
-
-      t.timestamps
-    end
-  end
-end
