73e2e832eba87e6668152e2b49a3e0ed1a6cdbb0
rquinterosv <rafael.quinterosv@gmail.com>
2022-02-15 20:39:11 -0300

    se agrego el modelo Order y Order_items, se modificaron los modelos de prodcuts y se crearon submodelos para valores y cantidades

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def current_order
+        if !session[:order_id].nill?
+            Order.find(session[:order_id])
+        else
+            Order.new
+        end
+
+    end
 end
