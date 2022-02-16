70f59033c90701871ae1607c41d0f7c3c5336fd7
rquinterosv <rafael.quinterosv@gmail.com>
2022-02-16 00:10:32 -0300

    se arreglo el pr problema para agregar el item al carro

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,11 +1,35 @@
 class OrderItemsController < ApplicationController
 
-    def create  
+
+
+
+
+    def create
         @order = current_order
         @order_item = @order.order_items.new(order_params)
         @order.save
-        session[:order_id] = @order_id
+        session[:order_id] = @order.id
     end
+
+
+
+    def update
+        @order = current_order
+        @order_item = @order.order_items.find(params[:id])
+        @order_item.update_attributes(order_params)
+        @order_items = current_order.order_items
+    end
+
+    def destroy
+        @order = current_order
+        @order_item = @order.order_items.find(params[:id])
+        @order_item.destroy
+        @order_items = current_order.order_items
+    end
+
+
+
+
 
     private
 
